c513957ec5d28ae2a0697d454fbf08821cc0d7d8
cr1pto <20729543+cr1pto@users.noreply.github.com>
2025-02-23 21:45:14 -0800

    cleanup

--- sampleweb/sampleweb/src/main/java/com/example/cr1pto/sampleweb/controller/CustomersController.java
+++ sampleweb/sampleweb/src/main/java/com/example/cr1pto/sampleweb/controller/CustomersController.java
@@ -1,6 +1,5 @@
 package com.example.cr1pto.sampleweb.controller;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
